ff5cf29edf45027b96b6d9ee5c50af5a93e97cfc
Zzzazik <juliia.chvaliuk@gmail.com>
2024-04-30 13:42:53 +1200

    Finished TODO's

--- TicTacToe/src/Default/Cell.java
+++ TicTacToe/src/Default/Cell.java
@@ -15,11 +15,14 @@ public class Cell {
 	public Cell(int row, int col) {
 		
 		// TODO: Initialise the variables row, col 
-
+		// Initialise the variables row, col 
+        this.row = row;
+        this.col = col;
 		
 		
 		//TODO: call the method that sets the cell content to EMPTY
-		 
+        // Call the method that sets the cell content to EMPTY
+        clear(); 
 	}
 	
 
@@ -48,7 +51,8 @@ public class Cell {
 	public void clear() {
 		
 		// TODO: Set the value of content to Empty (Remember this is an enum)
-
+		// Set the value of content to Empty (Remember this is an enum)
+        content = Player.Empty;
 		
 	}
 		
